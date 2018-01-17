e04877fe06b3eb9d2375ae6114110d6d05bcddfe
Kevin Grandon <kevingrandon@yahoo.com>
2018-01-16 18:02:24 -0800

    Fix lint

--- src/__tests__/index.node.js
+++ src/__tests__/index.node.js
@@ -3,6 +3,6 @@
 process.on('unhandledRejection', e => {
   throw e;
 });
-import "./__node__/tests/prepare-render.js";
-import "./__node__/tests/prepare-context.js";
-import "./__node__/tests/split.js";
+import './__node__/tests/prepare-render.js';
+import './__node__/tests/prepare-context.js';
+import './__node__/tests/split.js';
