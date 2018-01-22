9ce09591835402d827f0deec4c3f2e7a91dc3469
Alexander Smith <amsmith@uber.com>
2018-01-22 13:39:34 -0800

    Add short MIT license header to all source files

--- src/traverse-exclude.js
+++ src/traverse-exclude.js
@@ -1,3 +1,9 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import prepared from './prepared.js';
 
 // Stops the traversal at this node. Useful for optimizing the prepare traversal
