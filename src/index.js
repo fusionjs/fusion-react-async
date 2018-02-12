c64d4bfb6e3c556a144c8322ff09a58f6fff9ed9
Kevin Grandon <kevingrandon@yahoo.com>
2018-02-11 16:08:21 -0800

    Implement minimial flow support
    While this won't provide full coverage, it will allow folks to use flow without ignoring fusion imports.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// @flow
 import dispatched from './dispatched';
 import prepare from './prepare';
 import prepared from './prepared';
