d9fbc1d30fb22f1f50d84b54b7b4f68a622ebe90
Alexander Smith <amsmith@uber.com>
2018-04-12 13:33:49 -0700

    Improve Flow type definitions for exported functions

--- src/index.js
+++ src/index.js
@@ -2,15 +2,54 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
  */
 
-// @flow
+import * as React from 'react';
+
 import dispatched from './dispatched';
 import prepare from './prepare';
 import prepared from './prepared';
 import split from './split';
 import exclude from './traverse-exclude';
-import middleware from './middleware';
+import middleware from './middleware'; //typed
+
+const prepareTyped: (
+  element: React.Element<any>
+) => Promise<React.ComponentType<any>> = prepare;
+
+const preparedTyped: (
+  sideEffect: (props: Object, context: Object) => Promise<any>,
+  opts?: {
+    defer?: boolean,
+    boundary?: boolean,
+    componentDidMount?: boolean,
+    componentWillReceiveProps?: boolean,
+    componentDidUpdate?: boolean,
+    forceUpdate?: boolean,
+    contextTypes?: Object,
+  }
+) => (
+  Component: React.ComponentType<any>
+) => React.ComponentType<any> = prepared;
+
+const splitTyped: (opts: {
+  load: () => Promise<any>,
+  LoadingComponent: React.ComponentType<any>,
+  ErrorComponent: React.ComponentType<any>,
+}) => React.ComponentType<any> = split;
+
+const excludeTyped: (
+  Component: React.ComponentType<any>
+) => React.ComponentType<any> = exclude;
 
 // TODO(#3): Can we get ride of some of these exports?
-export {dispatched, prepare, prepared, split, exclude, middleware};
+export {
+  dispatched,
+  prepareTyped as prepare,
+  preparedTyped as prepared,
+  splitTyped as split,
+  excludeTyped as exclude,
+  middleware,
+};
